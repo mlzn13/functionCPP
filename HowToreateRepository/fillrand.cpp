cf8a651d75ec447875a9873aff2ac9ac86aa7997
mlzn13 <marinalizun@gmail.com>
2020-03-23 20:26:14 +0200

    every function in own file

---
+++ HowToreateRepository/fillrand.cpp
@@ -0,0 +1,35 @@
+#include"functions.h"
+
+
+void FillRand(int arr[], const int n)
+{
+	//cout << typeid(arr).name() << endl;
+	for (int i = 0; i < n; i++)
+	{
+		arr[i] = rand() % 100;
+	}
+}
+
+void FillRand(double arr[], const int n)
+{
+	//cout << typeid(arr).name() << endl;
+	for (int i = 0; i < n; i++)
+	{
+		arr[i] = double(rand() % 100) / 10;
+		//arr[i] /= 10;
+	}
+}
+
+void FillRand(int arr[ROWS][COLS], const int ROWS, const int COLS)
+{
+	for (int i = 0; i < ROWS; i++)
+	{
+		for (int j = 0; j < COLS; j++)
+		{
+			arr[i][j] = rand() % 100;
+		}
+	}
+}
+
+
+
